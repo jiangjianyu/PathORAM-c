8976e89b9ec155b017f21555373f2e67fa03b233
maxxie <jyst7275@gmail.com>
2016-05-02 10:45:25 +0800

    debug

---
+++ src/socket.c
@@ -0,0 +1,12 @@
+//
+// Created by jyjia on 2016/5/2.
+//
+
+void sock_init(struct sockaddr_in *addr, socklen_t *addrlen, int *sock,
+               char *host, int port) {
+    inet_aton(host, &addr->sin_addr);
+    addr->sin_port = htons(port);
+    addr->sin_family = AF_INET;
+    *sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    *addrlen = sizeof(struct sockaddr_in);
+}
--- src/socket.h
+++ src/socket.h
@@ -72,12 +72,5 @@ typedef struct {
 #define ORAM_SOCKET_BLOCK_SIZE_R sizeof(socket_read_block_r) + sizeof(int)
 
 void sock_init(struct sockaddr_in *addr, socklen_t *addrlen, int *sock,
-                 char *host, int port) {
-    inet_aton(host, &addr->sin_addr);
-    addr->sin_port = htons(port);
-    addr->sin_family = AF_INET;
-    *sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    *addrlen = sizeof(struct sockaddr_in);
-}
-
+                 char *host, int port);
 #endif //PATHORAM_SOCKET_H
